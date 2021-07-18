f0d6712ad2a4500d201c0e7c875d7e26ca11d4ca
Quan Nguyen <42907738+quanmario0311@users.noreply.github.com>
2021-07-18 14:57:55 -0700

    Update train_cifar10.py

--- train_cifar10.py
+++ train_cifar10.py
@@ -18,7 +18,7 @@ train_len = len(train_data)
 val_len = test_len = int(len(test_val_data)/2)
 test_data, val_data = torch.utils.data.random_split(test_val_data, [test_len, val_len])
 num_class = len(np.unique(train_data.targets))
-train_loader = DataLoader(dataset = train_data, batch_size = 128, shuffle = True, drop_last=True, pin_memory=True, num_workers=36)
+train_loader = DataLoader(dataset = train_data, batch_size = 128, shuffle = True, drop_last=True, pin_memory=True, num_workers=4)
 test_loader = DataLoader(dataset = test_data, batch_size=32)
 valid_loader = DataLoader(dataset = val_data, batch_size= 16)
 
