76d1c3528bc04d85c4b2d8795f63bfbc1afef9b1
Nguyen Hoang Quan <quanjenkey@gmail.com>
2021-07-19 13:24:36 -0700

    update

--- finetune.py
+++ finetune.py
@@ -10,7 +10,7 @@ import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
 from SimCLR import SimCLR
 
-backbone = SimCLR.load_from_checkpoint('./saved_models/simclr-epoch=12-avg_train_loss=4.86.ckpt', strict=False)
+backbone = SimCLR.load_from_checkpoint('./models/simclr-epoch=31-avg_train_loss=2.77-v1.ckpt', strict=False)
 
 train_transform = train_transform(size=224)
 val_test_transform = val_test_transform(size=224)
@@ -20,9 +20,9 @@ test_val_data = CIFAR10(root="./cifar10",train = False,transform=val_test_transf
 val_len = test_len = int(len(test_val_data)/2)
 test_data, val_data = torch.utils.data.random_split(test_val_data, [test_len, val_len])
 num_class = len(np.unique(train_data.targets))
-train_loader = DataLoader(dataset = train_data, batch_size = 8, shuffle = True, drop_last=True)
+train_loader = DataLoader(dataset = train_data, batch_size = 64, shuffle = True, num_workers = 12, drop_last=True, pin_memory=True)
 test_loader = DataLoader(dataset = test_data, batch_size=32)
-valid_loader = DataLoader(dataset = val_data, batch_size= 8, drop_last=True)
+valid_loader = DataLoader(dataset = val_data, batch_size= 32, drop_last=True,pin_memory=True)
 
 finetuner = SSLFineTuner(backbone, in_features=backbone.hidden_mlp, num_classes=num_class, hidden_dim=1024)
 checkpoint_callback = ModelCheckpoint(
